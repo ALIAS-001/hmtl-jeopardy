c1497a6a3e48b86b6d66ad1d2182eca5cda57fae
ALIAS-001 <53192072+ALIAS-001@users.noreply.github.com>
2019-08-22 15:15:15 -0700

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,27 @@
+var variables = [
+	[
+		//questions
+		["question 1-2", "question 1-4", "question 1-6", "question 1-8", "question 1-10"],
+		["question 2-2", "question 2-4", "question 2-6", "question 2-8", "question 2-10"],
+		["question 3-2", "question 3-4", "question 3-6", "question 3-8", "question 3-10"],
+		["question 4-2", "question 4-4", "question 4-6", "question 4-8", "question 4-10"],
+	],
+	[
+		//answers
+		["asnwer 1-2", "answer 1-4", "answer 1-6", "answer 1-8", "answer 1-10", "answer 1-12"],
+		["answer 2-2", "answer 2-4", "answer 2-6", "answer 2-8", "answer 2-10", "answer 2-12"],
+		["answer 3-2", "answer 3-4", "answer 3-6", "answer 3-8", "answer 3-10", "answer 3-12"],
+		["answer 4-2", "answer 4-4", "answer 4-6", "answer 4-8", "answer 4-10", "answer 4-12"],
+	],
+	[
+		//topic names
+		["topic 1", "topic 2", "topic 3", "topic 4"]
+	],
+	[
+		//final jeopardy Q&A				worth double of the randomized points
+		["final question", "final answer", "double question", "double answer"]
+	]
+];
+//point multiplier
+var ratio = 100;
+var startp = 1000;
